ac817a8403e9889949030d67b2e46d715ab8f062
unbounded3 <jiaenpang54@gmail.com>
2022-05-02 21:01:36 +0800

    修改了文件名称，命名按照一个表一个dao和一个实现类的规则

--- src/main/java/cn/edu/guet/WeShop/manager/ReplenishManager.java
+++ src/main/java/cn/edu/guet/WeShop/manager/ReplenishManager.java
@@ -21,7 +21,6 @@ public class ReplenishManager {
     double price = 0;
     double stock;
     List<String> title = new ArrayList<>();
-    
 
     public ReplenishManager(boolean flag,double stock){
         this.flag = flag;
