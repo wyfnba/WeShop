4dd53071f0a7877383fee022ad4ac9fe9b85b8da
wyfnba <2076500621@qq.com>
2022-05-06 17:32:29 +0800

    修正

--- src/main/java/cn/edu/guet/WeShop/dao/ItemStockDao.java
+++ src/main/java/cn/edu/guet/WeShop/dao/ItemStockDao.java
@@ -8,12 +8,13 @@ package cn.edu.guet.WeShop.dao;
 import cn.edu.guet.WeShop.bean.Item_stock;
 
 import java.sql.SQLException;
+import java.util.List;
 
 /**
  * 进货时增加库存
  */
 public interface ItemStockDao {
     void insertItemStock(Item_stock item_stock) throws SQLException;
-    void increaseItemStock(List<Item_stock> item_stock) throws SQLException;
+    void increaseItemStock(Item_stock item_stock) throws SQLException;
     void decreaseItemStock(Item_stock item_stock) throws SQLException;
 }
